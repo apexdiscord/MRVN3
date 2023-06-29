c53490b32320c871243df8020e596ed4b2685b19
SDCore <sdcored@gmail.com>
2023-06-29 14:55:12 -0500

    Refactor /lfg-mixtape

--- functions/utilities.js
+++ functions/utilities.js
@@ -18,7 +18,7 @@ function setVCLimit(mode, channel) {
 		}
 
 		return;
-	} else if (mode == 'Trios' || mode == 'LTM') {
+	} else if (mode == 'Trios' || mode == 'LTM' || mode == 'Mixtape') {
 		channel.member.voice.channel.setUserLimit(3);
 
 		if (channel.member.voice.channel.userLimit != 3) {

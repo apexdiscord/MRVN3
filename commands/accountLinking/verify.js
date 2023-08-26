740b93593b6193f4caf7ec34c15fc5b5d4597ea1
SDCore <sdcored@gmail.com>
2023-08-26 17:55:13 -0500

    Notes

--- commands/accountLinking/verify.js
+++ commands/accountLinking/verify.js
@@ -6,5 +6,21 @@ module.exports = {
 
 	async execute(interaction) {
 		await interaction.deferReply({ ephemeral: true });
+
+		// select the user account from the api
+		// select the linked account form the database
+		// if the account matches, start verification
+
+		// check first tracker, api and database match, move onto second tracker
+		// check second tracker, api and database match, move onto third tracker
+		// check third tracker, api and database match, verification complete
+
+		// FIRST: add the discord id, player id, and platform to specter
+		// remove the temp link from temp_linking
+		// send an embed, "success!!!!!!"
+
+		// if any of the trackers don't match, send an embed, "verification failed, please try again"
+		// ideally, it would be at which tracker the verification failed
+		// (if possible? maybe? kinda. a lotta effort :3)
 	},
 };

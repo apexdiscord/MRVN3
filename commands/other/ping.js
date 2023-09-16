0a581f7c91e550f07143bbd3ed4463937f7f3056
randomhuman9 <randomhumanbeing9@gmail.com>
2023-09-17 01:36:42 +0530

    Create ping.js
    initial code which does not seem to recognise created table for whatever reason

---
+++ commands/other/ping.js
@@ -0,0 +1,76 @@
+const moment = require('moment');
+const Database = require('better-sqlite3');
+const { SlashCommandBuilder } = require('discord.js');
+const { Table } = require('console-table-printer');
+
+const { timeoutController } = require('../../functions/utilities.js');
+const db_memberDecay = new Database(`${__dirname}/../../databases/memberDecay.sqlite`);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('add-ping-role')
+		.setDescription('Add a ranked ping role to get notified when someone posts looking for that rank.')
+		.addStringOption(option =>
+			option.setName('rank').setDescription('Choose the rank you want to be pinged for.').setRequired(true).addChoices(
+					{ name: 'Rookie', value: '1125310238514483230' },
+					{ name: 'Bronze', value: 'roleIDplaceholder' },
+					{ name: 'Silver', value: 'roleIDplaceholder' },
+					{ name: 'Gold', value: 'roleIDplaceholder' },
+					{ name: 'Platinum', value: 'roleIDplaceholder' },
+					{ name: 'Diamond', value: 'roleIDplaceholder' },
+					{ name: 'Master', value: 'roleIDplaceholder' },
+					{ name: 'Apex Predator', value: 'roleIDplaceholder' },
+				),
+		)
+		.addStringOption(option =>
+			option.setName('time').setDescription('How long do you want the role for?').setRequired(true).addChoices(
+				{
+					name: '20 minutes',
+					value: '1200',
+				},
+				{
+					name: '1 hour',
+					value: '3600',
+				},
+				{
+					name: 'Permanent',
+					value: 'Permanent',
+				},
+			),
+		),
+
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+
+		const roleToAdd = interaction.options.getString('rank');
+
+		const selectedTime = parseInt(interaction.options.getString('time'));
+
+		const userID = interaction.user.id;
+
+		try {
+			const timestamp = moment().unix();
+			
+			if (typeof selectedTime === 'number') {
+				const expiryTime = timestamp + selectedTime
+				db_memberDecay.prepare(`INSERT OR REPLACE INTO roleExpiryTracker (expiryTime, roleToAdd, userID) VALUES (?, ?, ?)`).run(expiryTime, roleToAdd, userID);
+				targetMember.roles.add(roleToAdd);
+
+			} else if (!isNaN(selectedTime)) {
+				targetMember.roles.add(roleToAdd);
+				  }
+			
+			else {
+				message.channel.send('An error occurred while adding the role.');
+				  }
+
+			await interaction.editReply({
+				content: `Successfully add the role <@${roleToAdd}> to you for thee selected time period!`,
+				ephemeral: true,
+			});
+		} catch (error) {
+			console.log(error);
+			await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	},
+};

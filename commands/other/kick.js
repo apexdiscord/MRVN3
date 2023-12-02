62cc85a61b266e6ecdc2d907edfbf8e4d91f27d2
SDCore <sdcored@gmail.com>
2023-12-02 04:05:45 -0600

    Add Optional Kick Reason

--- commands/other/kick.js
+++ commands/other/kick.js
@@ -10,10 +10,14 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kick')
 		.setDescription('Removes a user from your channel. You must have joined an empty channel first to use this command.')
-		.addUserOption(option => option.setName('member-name').setDescription('The username of the user you want to kick.').setRequired(true)),
+		.addUserOption(option => option.setName('member-name').setDescription('The username of the user you want to kick.').setRequired(true))
+		.addStringOption(option => option.setName('reason').setDescription('The reason for kicking the user.').setRequired(false)),
 
 	async execute(interaction) {
 		await interaction.deferReply({ ephemeral: true });
+
+		// Get reason for kick
+		const kickReason = interaction.options.getString('reason') || null;
 
 		// User to kick
 		const userToKick = interaction.options.getUser('member-name');
@@ -53,13 +57,13 @@ module.exports = {
 
 			// Set timeouts for kicked user
 			if (entryCount3 >= 9) {
-				timeoutController(2419199_000, 2419199000, memberToKick, interaction, entryCount1, entryCount2, entryCount3);
+				timeoutController(2419199_000, 2419199000, memberToKick, interaction, entryCount1, entryCount2, entryCount3, kickReason);
 			} else if (entryCount2 >= 6) {
-				timeoutController(3600_000, 3600000, memberToKick, interaction, entryCount1, entryCount2, entryCount3);
+				timeoutController(3600_000, 3600000, memberToKick, interaction, entryCount1, entryCount2, entryCount3, kickReason);
 			} else if (entryCount1 >= 3) {
-				timeoutController(600_000, 600000, memberToKick, interaction, entryCount1, entryCount2, entryCount3);
+				timeoutController(600_000, 600000, memberToKick, interaction, entryCount1, entryCount2, entryCount3, kickReason);
 			} else {
-				timeoutController(0, 0, memberToKick, interaction, entryCount1, entryCount2, entryCount3);
+				timeoutController(0, 0, memberToKick, interaction, entryCount1, entryCount2, entryCount3, kickReason);
 			}
 
 			// If the kick count is less than 2, don't show the "Message ModMail" portion of the kick message

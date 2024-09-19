aa384d0df2cfa508e3da7a38cb9c11e35586780d
SDCore <sdcored@gmail.com>
2024-09-19 17:10:11 -0500

    Update Stuff

--- commands/lfg/lfg-tournament.js
+++ commands/lfg/lfg-tournament.js
@@ -39,10 +39,10 @@ module.exports = {
 					name: '2',
 					value: '2',
 				},
-				{
-					name: '3',
-					value: '3',
-				},
+				// {
+				// 	name: '3',
+				// 	value: '3',
+				// },
 			),
 		)
 		.addStringOption(option =>
